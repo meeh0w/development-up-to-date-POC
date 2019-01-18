468e99e145ddcc8e85aeadd9ed6b18d32c016ddd
Damian Wisniewski <damian.wisniewski@performgroup.com>
2019-01-18 15:20:03 +0100

    changes git #9

--- git.js
+++ git.js
@@ -1,25 +1,33 @@
 const { execSync } = require('child_process')
 const chalk = require('chalk')
 const readline = require('readline')
+
 const promptConsole = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 })
 
+const askToAbort = () => {
+    promptConsole.question('\n Would you like to abort operation? [Y/N] ', (answer) => {
+        if (answer.toLowerCase() !== 'n') {
+            execSync(`git rebase --abort`)
+        }
+        promptConsole.close();
+    })
+}
+
 try {
     execSync(`git rebase master development`)
 } catch(err) {
-    console.log('\n-----------------------------')
+    console.log('\nrun "git rebase master development"\n')
     console.log('\n', err.output[1].toString())
-    console.log('\n-----------------------------')
-    console.log('\n', err.output[2].toString())
-    console.log(chalk.bgRed.white('!!! > BIG-ASS RED MESSAGE < !!!'))
+    console.log(chalk.bgRed.white('\n !!!  WATCH OUT  !!! \n'))
+    console.log(chalk.red('\n You are  \n'))
+    console.log(chalk.bgRed.white(err.output[2].toString()))
 
-    promptConsole.question('Would you like to abort rebase? [Y/N] ', (answer) => {
-        if (answer.toLowerCase() === 'y') {
-            execSync(`git rebase --abort`)
-        }
+    if (err.output[2].toString().search('CONFLICT') > -1) {
+        askToAbort()
+    }
 
-        promptConsole.close();
-    });
+    process.exit(0)
 }
