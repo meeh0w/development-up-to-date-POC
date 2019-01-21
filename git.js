36b66c5021923e4df816bd3dafaadd10d241fcce
Michał Leszczyk <michal.leszczyk@performgroup.com>
2019-01-21 12:49:05 +0100

    add git.js

---
+++ git.js
@@ -0,0 +1,25 @@
+const { execSync } = require('child_process')
+const chalk = require('chalk')
+
+try {
+    const output = execSync(`git rebase master development`).toString()
+    console.log(output)
+} catch(err) {
+    const firstOutput = err.output[1].toString()
+    const secondOutput = err.output[2].toString()
+
+    console.log('\nrun: "git rebase master development"\n')
+
+    console.log('\n', firstOutput)
+    console.log(chalk.bgRed.white('\n !!!  WATCH OUT  !!! '))
+    console.log('\n You are trying to rebase master onto development \n')
+    console.log(chalk.bgRed.white(secondOutput))
+
+    if (firstOutput.search('CONFLICT') > -1) {
+        console.log(chalk.red('\n Some CONFLICTS was found! \n'))
+        execSync(`git rebase --abort`)
+        console.log('Rebase was aborted. Please rebase it manually!')
+    }
+
+    process.exit(1)
+}
